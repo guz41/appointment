612223446b4a09638da5d4c524ad3f39bb04ad99
guz41 <guz41@hotmail.com>
2015-08-12 10:24:43 +1000

    delete added

--- src/java/appointment/data/ObjectFactory.java
+++ src/java/appointment/data/ObjectFactory.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
-
 /**
  *
  * @author Marrows
@@ -18,7 +17,6 @@ import java.sql.*;
 public class ObjectFactory {
 
     // JDBC driver name and database URL
-
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://52.27.193.81/base";
     //  Database credentials
@@ -42,7 +40,7 @@ public class ObjectFactory {
             preparedStatement.setString(1, first_name);
             preparedStatement.setString(2, last_name);
             preparedStatement.executeUpdate();
-            
+
             String selectStatement;
             selectStatement = "SELECT * FROM customer WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "'";
             System.out.println(selectStatement);
@@ -56,7 +54,7 @@ public class ObjectFactory {
                 Long id = rs.getLong("cust_id");
 
                 returnCust = new Customer(first, last, id);
-                return returnCust;                
+                return returnCust;
             }
             // Clean-up environment
             rs.close();
@@ -80,13 +78,13 @@ public class ObjectFactory {
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException se) { 
+            } catch (SQLException se) {
+            }
         }
-    }
         return null;
-}
-    
-     public Customer getCustomer(Customer cust) {
+    }
+
+    public Customer getCustomer(Customer cust) {
         Connection conn = null;
         Statement stmt = null;
         Customer returnCust = null;
@@ -96,7 +94,7 @@ public class ObjectFactory {
             stmt = conn.createStatement();
             String first_name = cust.getFirstName();
             String last_name = cust.getLastName();
-          
+
             String selectStatement;
             selectStatement = "SELECT * FROM customer WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "'";
             ResultSet rs = stmt.executeQuery(selectStatement);
@@ -109,7 +107,7 @@ public class ObjectFactory {
                 Long id = rs.getLong("cust_id");
 
                 returnCust = new Customer(first, last, id);
-                return returnCust;                
+                return returnCust;
             }
             // Clean-up environment
             rs.close();
@@ -133,22 +131,22 @@ public class ObjectFactory {
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException se) { 
+            } catch (SQLException se) {
+            }
         }
-    }
         return null;
-}
-     
-     public Customer getCustomer(Long ID) {
+    }
+
+    public Customer getCustomer(Long ID) {
         Connection conn = null;
         Statement stmt = null;
         Customer returnCust = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();          
+            stmt = conn.createStatement();
             String selectStatement;
-            selectStatement = "SELECT * FROM customer WHERE cust_id = '" + ID +"'";
+            selectStatement = "SELECT * FROM customer WHERE cust_id = '" + ID + "'";
             ResultSet rs = stmt.executeQuery(selectStatement);
 
             // Extract data from result set
@@ -159,7 +157,7 @@ public class ObjectFactory {
                 Long id = rs.getLong("cust_id");
 
                 returnCust = new Customer(first, last, id);
-                return returnCust;                
+                return returnCust;
             }
             // Clean-up environment
             rs.close();
@@ -183,13 +181,51 @@ public class ObjectFactory {
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException se) { 
+            } catch (SQLException se) {
+            }
         }
-    }
         return null;
-}
-     
-      public List<Customer> getAllCustomers() {
+    }
+    
+        public void deleteCustomer(Long ID) {
+        Connection conn = null;
+        Statement stmt = null;
+        Customer returnCust = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            String selectStatement;
+            selectStatement = "Delete * FROM customer WHERE cust_id = '" + ID + "'";
+            
+            
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+
+        } catch (ClassNotFoundException e) {
+            //Handle errors for Class.forName
+
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }
+        }
+        
+    }
+
+    public List<Customer> getAllCustomers() {
         Connection conn = null;
         Statement stmt = null;
         Customer returnCust = null;
@@ -199,7 +235,6 @@ public class ObjectFactory {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = conn.createStatement();
 
-          
             String selectStatement;
             selectStatement = "SELECT * FROM customer";
             ResultSet rs = stmt.executeQuery(selectStatement);
@@ -210,9 +245,9 @@ public class ObjectFactory {
                 String first = rs.getString("first_name");
                 String last = rs.getString("last_name");
                 Long id = rs.getLong("cust_id");
-                allCustomers.add(new Customer(first, last, id));                             
+                allCustomers.add(new Customer(first, last, id));
             }
-               
+
             // Clean-up environment
             rs.close();
             stmt.close();
@@ -236,15 +271,14 @@ public class ObjectFactory {
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException se) { 
+            } catch (SQLException se) {
+            }
         }
-    }
         return null;
-}
+    }
 
-    
     public static void main(String[] args) {
-        Customer test1 = new Customer("Test3","Test3",(long) 1);
+        Customer test1 = new Customer("Test3", "Test3", (long) 1);
         Customer test2;
         ObjectFactory_tutorial a = new ObjectFactory_tutorial();
         //test2 = a.setCustomer(test1);
@@ -252,6 +286,6 @@ public class ObjectFactory {
         test2 = a.getAllCustomers().get(2);
 
         System.out.println(test2.getFirstName() + " " + test2.getLastName() + " " + test2.getID());
-                
+
     }
 }
